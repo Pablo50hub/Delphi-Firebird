46fcc743a0716643833044730d7be131d929cb1c
Pablo Saldanha <menezespablo50@gmail.com>
2022-12-09 10:20:58 -0400

    Aula 2

---
+++ Projeto Delphi/ERPDelphi/src/views/View.principal.pas
@@ -0,0 +1,32 @@
+unit View.principal;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ExtCtrls;
+
+type
+  TViewPrincipal = class(TForm)
+    pnlTopo: TPanel;
+    btnFechar: TButton;
+    procedure btnFecharClick(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  ViewPrincipal: TViewPrincipal;
+
+implementation
+
+{$R *.dfm}
+
+procedure TViewPrincipal.btnFecharClick(Sender: TObject);
+begin
+  Application.Terminate;
+end;
+
+end.
