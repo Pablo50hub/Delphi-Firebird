d11131218e29369193ae6a09f4e1c58c6e04c484
Pablo Saldanha <menezespablo50@gmail.com>
2022-12-09 10:28:45 -0400

    Aula 6

--- Projeto Delphi/ERPDelphi/ERPCurso.dpr
+++ Projeto Delphi/ERPDelphi/ERPCurso.dpr
@@ -5,7 +5,8 @@ uses
   View.principal in 'src\views\View.principal.pas' {ViewPrincipal},
   Service.conexao in 'src\services\Service.conexao.pas' {ServiceConexao: TDataModule},
   Service.cadastro in 'src\services\Service.cadastro.pas' {ServiceCadastro: TDataModule},
-  Provider.constants in 'src\providers\Provider.constants.pas';
+  Provider.constants in 'src\providers\Provider.constants.pas',
+  View.base in 'src\views\View.base.pas' {ViewBase};
 
 {$R *.res}
 
@@ -13,7 +14,7 @@ begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TViewPrincipal, ViewPrincipal);
-  Application.CreateForm(TServiceConexao, ServiceConexao);
   Application.CreateForm(TServiceCadastro, ServiceCadastro);
+  Application.CreateForm(TServiceConexao, ServiceConexao);
   Application.Run;
 end.
