2a0967f181f020d3e62d429507b4df64b35840da
Pablo Saldanha <menezespablo50@gmail.com>
2022-12-09 10:25:51 -0400

    Aula 3 parte 9

--- Projeto Delphi/ERPDelphi/src/views/View.principal.pas
+++ Projeto Delphi/ERPDelphi/src/views/View.principal.pas
@@ -5,7 +5,8 @@ interface
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ExtCtrls, Vcl.Buttons,
-  Vcl.Imaging.pngimage;
+  Vcl.Imaging.pngimage, System.ImageList, Vcl.ImgList, System.Actions,
+  Vcl.ActnList;
 
 type
   TViewPrincipal = class(TForm)
